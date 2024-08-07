f43ea2abea1e5e78cf23d821f47bdc7edcd3188d
Awais Rasheed <46028223+Awais-Rasheed@users.noreply.github.com>
2024-08-06 23:03:59 -0700

    Video Editor

---
+++ VideoEditor/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace VideoEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Start = new System.Windows.Forms.Button();
+            this.Merging = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.play = new System.Windows.Forms.Button();
+            this.part = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Start
+            // 
+            this.Start.BackColor = System.Drawing.Color.Black;
+            this.Start.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Start.ForeColor = System.Drawing.Color.White;
+            this.Start.Location = new System.Drawing.Point(26, 74);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(75, 32);
+            this.Start.TabIndex = 0;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = false;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // Merging
+            // 
+            this.Merging.BackColor = System.Drawing.Color.Black;
+            this.Merging.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Merging.ForeColor = System.Drawing.Color.White;
+            this.Merging.Location = new System.Drawing.Point(143, 393);
+            this.Merging.Name = "Merging";
+            this.Merging.Size = new System.Drawing.Size(75, 32);
+            this.Merging.TabIndex = 1;
+            this.Merging.Text = "Merging";
+            this.Merging.UseVisualStyleBackColor = false;
+            this.Merging.Click += new System.EventHandler(this.Merging_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.part);
+            this.panel1.Controls.Add(this.Start);
+            this.panel1.Location = new System.Drawing.Point(13, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(124, 360);
+            this.panel1.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.axWindowsMediaPlayer1);
+            this.panel2.Location = new System.Drawing.Point(143, 13);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(636, 360);
+            this.panel2.TabIndex = 3;
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(0, 3);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(636, 354);
+            this.axWindowsMediaPlayer1.TabIndex = 0;
+            // 
+            // play
+            // 
+            this.play.BackColor = System.Drawing.Color.Black;
+            this.play.FlatAppearance.BorderSize = 0;
+            this.play.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.play.ForeColor = System.Drawing.Color.White;
+            this.play.Location = new System.Drawing.Point(224, 393);
+            this.play.Name = "play";
+            this.play.Size = new System.Drawing.Size(77, 32);
+            this.play.TabIndex = 1;
+            this.play.Text = "Play";
+            this.play.UseVisualStyleBackColor = false;
+            this.play.Click += new System.EventHandler(this.play_Click);
+            // 
+            // part
+            // 
+            this.part.Location = new System.Drawing.Point(4, 48);
+            this.part.Name = "part";
+            this.part.Size = new System.Drawing.Size(117, 20);
+            this.part.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(-1, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Chunk Length in Seconds";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Yellow;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.play);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.Merging);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.Button Merging;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.Button play;
+        private System.Windows.Forms.TextBox part;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ VideoEditor/Form1.cs
@@ -0,0 +1,325 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Speech.Synthesis;
+//using AForge.Video.FFMPEG;
+using NAudio.Wave;
+using System.Windows.Forms;
+using System.IO;
+using Accord.Video.FFMPEG;
+using Accord.Audio;
+using Accord.Audio.Formats;
+using Accord.Video;
+using System.Diagnostics;
+using System.Drawing.Drawing2D;
+
+namespace VideoEditor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            SetRoundedButton(play, 20);
+            SetRoundedButton(Start, 20);
+            SetRoundedButton(Merging, 20);
+
+            SetBorderlessButton(play);
+            SetBorderlessButton(Start);
+            SetBorderlessButton(Merging);
+
+            
+        }
+
+        public void ConvertVideoToImages(string videoPath, string outputFolder)
+        {
+            Form1 obj = new Form1();
+            using (VideoFileReader videoReader = new VideoFileReader())
+            {
+                // Open the video file
+                videoReader.Open(videoPath);
+
+                // Create output folder if it doesn't exist
+                if (!Directory.Exists(outputFolder))
+                {
+                    Directory.CreateDirectory(outputFolder);
+                }
+                
+
+                // Loop through each frame in the video
+
+                int chunkDurationInSeconds;
+                if (!int.TryParse(part.Text, out chunkDurationInSeconds) || chunkDurationInSeconds <= 0)
+                {
+                    MessageBox.Show("Invalid chunk length. Please enter a positive integer.");
+                    return;
+                }
+
+                int frameRate = 30;
+                
+                int framesPerChunk = frameRate * chunkDurationInSeconds;
+
+                double totalFrames = videoReader.FrameCount;
+
+                for (int i = 0; i < totalFrames; i += framesPerChunk)
+                {
+                    // Read frames for the next 5 seconds
+                    for (int j = 0; j < framesPerChunk && (i + j) < totalFrames; j++)
+                    {
+                        Bitmap videoFrame = videoReader.ReadVideoFrame();
+
+                        // Save the frame as an image
+                        string imagePath = Path.Combine(outputFolder, $"chunk_{i / frameRate:D5}", $"frame_{i + j:D5}.png");
+                        Directory.CreateDirectory(Path.GetDirectoryName(imagePath)); // Ensure the directory exists
+                        videoFrame.Save(imagePath, ImageFormat.Png);
+
+                        // Release the frame
+                        videoFrame.Dispose();
+                    }
+                }
+
+                // Close the video file
+                videoReader.Close();
+            }
+        }
+
+        public static void CreateVideo(string audioFilePath, string imageFolder, string outputVideoPath)
+        {
+            // Create an audio reader with the specified WAV file
+            using (WaveFileReader audioReader = new WaveFileReader(audioFilePath))
+            {
+                // Desired video duration in seconds
+                double desiredDuration = 10.0;
+
+                // Calculate the total number of frames based on the frame rate
+                int totalFrames = (int)(desiredDuration * 30); // Assuming 30 frames per second
+
+                // Get a list of image files in the specified folder
+                string[] imageFiles = Directory.GetFiles(imageFolder, "*.png");
+
+                // Adjust the number of frames to the minimum between totalFrames and imageFiles.Length
+                int framesToWrite = Math.Min(totalFrames, imageFiles.Length);
+
+                using (VideoFileWriter writer = new VideoFileWriter())
+                {
+                    try
+                    {
+                        writer.Open(outputVideoPath, 1920, 1080, 30, VideoCodec.MPEG4);
+
+                        // Set the video duration based on the number of frames
+                        double videoDuration = framesToWrite / 30.0; // Assuming 30 frames per second
+
+                        // Write each image frame to the video
+                        for (int i = 0; i < framesToWrite; i++)
+                        {
+                            Bitmap frame = new Bitmap(imageFiles[i]);
+                            writer.WriteVideoFrame(frame);
+
+                            // Release the frame
+                            frame.Dispose();
+                        }
+
+                        // Convert audio duration to the number of frames
+                        int audioFrameCount = (int)(audioReader.TotalTime.TotalSeconds * audioReader.WaveFormat.SampleRate);
+
+                        // Write the audio samples to the video using the hypothetical WriteAudioSamples method
+                        byte[] audioBuffer = new byte[audioFrameCount * audioReader.WaveFormat.BlockAlign];
+                        int bytesRead = audioReader.Read(audioBuffer, 0, audioBuffer.Length);
+                        //writer.WriteAudioSamples(audioBuffer, 0, bytesRead);
+
+                        Console.WriteLine($"Video created at: {outputVideoPath}");
+                    }
+                    finally
+                    {
+                        // Make sure to close the writer to release resources
+                        writer.Close();
+                    }
+                }
+            }
+        }
+
+        // Text to audio converter
+        public static void GenerateWavFromText(string text, string outputFilePath)
+        {
+            using (SpeechSynthesizer synth = new SpeechSynthesizer())
+            {
+                // Set the voice and rate (optional)
+                synth.SelectVoiceByHints(VoiceGender.Female, VoiceAge.Adult);
+                synth.Rate = 0; // Adjust the rate as needed
+
+                // Set the output format to WAV
+                synth.SetOutputToWaveFile(outputFilePath);
+
+                // Convert text to speech and save to the specified file
+                synth.Speak(text);
+            }
+
+            Console.WriteLine($"WAV file generated at: {outputFilePath}");
+        }
+
+        // Function to add subtitle to an image
+        private static void AddSubtitleToImage(string inputImagePath, string outputImagePath, string subtitleText)
+        {
+            using (Bitmap originalImage = new Bitmap(inputImagePath))
+            {
+                using (Graphics graphics = Graphics.FromImage(originalImage))
+                {
+                    Font subtitleFont = new Font("Arial", 12, FontStyle.Regular);
+                    SolidBrush subtitleBrush = new SolidBrush(Color.White);
+                    PointF subtitlePosition = new PointF(10, originalImage.Height - 30);
+
+                    graphics.DrawString(subtitleText, subtitleFont, subtitleBrush, subtitlePosition);
+                    originalImage.Save(outputImagePath, ImageFormat.Png);
+                }
+            }
+        }
+
+        public static void MergeVideoWithAudio(string videoFilePath, string audioFilePath, string outputFilePath)
+        {
+            // Specify the path to the FFmpeg executable
+            string ffmpegPath = @"C:\ffmpeg\bin\ffmpeg.exe"; // Replace with the actual path
+
+            // Use FFmpeg to merge audio and video
+            Process ffmpegProcess = new Process
+            {
+                StartInfo =
+            {
+                FileName = ffmpegPath,
+                Arguments = $"-i \"{videoFilePath}\" -i \"{audioFilePath}\" -c:v copy -c:a aac -strict experimental -map 0:v:0 -map 1:a:0 \"{outputFilePath}\"",
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            },
+                EnableRaisingEvents = true
+            };
+
+            ffmpegProcess.ErrorDataReceived += (sender, e) =>
+            {
+                if (!string.IsNullOrEmpty(e.Data))
+                {
+                    Console.WriteLine($"FFmpeg Error: {e.Data}");
+                }
+            };
+
+            ffmpegProcess.OutputDataReceived += (sender, e) =>
+            {
+                if (!string.IsNullOrEmpty(e.Data))
+                {
+                    Console.WriteLine($"FFmpeg Output: {e.Data}");
+                }
+            };
+
+            ffmpegProcess.Start();
+            ffmpegProcess.BeginErrorReadLine();
+            ffmpegProcess.BeginOutputReadLine();
+            ffmpegProcess.WaitForExit();
+        }
+
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            
+            
+            try
+            {
+                string videoPath = "D:\\Editor\\Sample.mp4";
+                string imageOutputFolder = "D:\\Editor\\ImageOuput";
+                string audioOutputFolder = "D:\\Editor\\AudioOutput";
+                string finalVideoPath = "D:\\Editor\\FinalVideo\\final_video.mp4";
+                string subtitleText = "This is Subtitle Text";
+                MessageBox.Show("Video generation Start.");
+                ConvertVideoToImages(videoPath, imageOutputFolder);
+
+                MessageBox.Show("Image generation Complete.");
+
+                GenerateWavFromText(subtitleText, $"{audioOutputFolder}/subtitle_audio.wav");
+
+                MessageBox.Show("Subtitle generation Complete.");
+                foreach (string imagePath in Directory.GetFiles(imageOutputFolder, "*.png"))
+                {
+                    AddSubtitleToImage(imagePath, imagePath, subtitleText);
+                }
+                MessageBox.Show("Video generation Start.");
+
+                CreateVideo($"{audioOutputFolder}/subtitle_audio.wav", imageOutputFolder, finalVideoPath);
+
+                MessageBox.Show("Video generation complete.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error in Start_Click: {ex.Message}");
+            }
+        }
+
+        private void Merging_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            file.ShowDialog();
+            file.Filter = "MP4 Files | *.mp4";
+            string videoFilePath = file.FileName;
+
+            OpenFileDialog Afile = new OpenFileDialog();
+            Afile.ShowDialog();
+            Afile.Filter = "MP3 Files | *.mp3";
+            string audioFilePath = Afile.FileName;
+            
+            if(videoFilePath == "" ||  audioFilePath == "")
+            {
+                MessageBox.Show("Audio or video file is missing");
+            }
+            else
+            {
+                string outputFilePath = @"D:\Editor\video1.mp4";
+
+                MergeVideoWithAudio(videoFilePath, audioFilePath, outputFilePath);
+
+                MessageBox.Show("Audio and video merged successfully.");
+            }
+
+            
+        }
+
+        private void Chunk_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void play_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            file.ShowDialog();
+            string path = file.FileName;
+            axWindowsMediaPlayer1.URL = path;
+        }
+
+        private void siticoneGradientButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SetRoundedButton(Button button, int borderRadius)
+        {
+            GraphicsPath path = new GraphicsPath();
+            path.AddArc(0, 0, borderRadius, borderRadius, 180, 90);
+            path.AddArc(button.Width - borderRadius, 0, borderRadius, borderRadius, 270, 90);
+            path.AddArc(button.Width - borderRadius, button.Height - borderRadius, borderRadius, borderRadius, 0, 90);
+            path.AddArc(0, button.Height - borderRadius, borderRadius, borderRadius, 90, 90);
+            path.CloseFigure();
+            button.Region = new Region(path);
+
+            button.Paint += (sender, e) =>
+            {
+                e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+            };
+        }
+
+        private void SetBorderlessButton(Button button)
+        {
+            button.FlatStyle = FlatStyle.Flat;
+            button.FlatAppearance.BorderSize = 0;
+            //button.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(0, 255, 255, 255); // Transparent border color
+            
+        }
+    }
+}
